b66a71ed1af9f3d044c852832a498031f3d32075
Iban Montero <imontero@student.42urduliz.com>
2023-12-18 20:41:59 +0100

    Add files via upload

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imontero <imontero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 17:06:28 by imontero          #+#    #+#             */
+/*   Updated: 2023/12/18 20:36:00 by imontero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int 		_hitPoints;
+		int 		_energyPoints;
+		int 		_attackDamage;
+
+	public:
+		ClapTrap(void);
+		ClapTrap(std::string name);
+		~ClapTrap(void);
+		
+		ClapTrap(const ClapTrap &copy);
+		
+		ClapTrap & operator =(ClapTrap const &inst2);
+		
+		void	attack(const std::string &target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+		
+		bool	isWorking(int hP, int eP);
+		
+};
+
+#endif
